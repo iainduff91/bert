85ca4c8152f2e1d5734d538c772be31249036cef
iain_duff <iain_duff@hotmail.co.uk>
2017-09-30 15:00:03 +0100

    Fix zip command in update_bert.sh

--- update_bert.sh
+++ update_bert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Zip up all JS files in bert
-zip -rX zips/bert.zip . -i \*.js
+zip -rX zips/bert.zip . -i node_modules/*.* *.js *.json
 
 # Update BertLambda with new index.js version
 aws lambda update-function-code --function-name BertLambda --zip-file fileb://zips/bert.zip
