1d50f59c62860540c1254cc984f1e21fa1aa0cb4
iain_duff <iain_duff@hotmail.co.uk>
2017-10-06 22:13:44 +0100

    *WORK IN PROGRESS* Implement function to retrieve all backlog cards untouched from 2 weeks ago

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 'use strict';
 
 let trello = require('./trello_wrapper');
+let async = require('async');
 
 exports.handler = (event, context, callback) => {
     //Run the lambda function on a schedule
     //When that schedule is triggered...
         //Get all cards that are over 2 weeks old
+        async.waterfall([
+            (next) => {
+                //Get all cards that are over 2 weeks old
+                trello.getOldBacklogCards((err, data) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    return next(null, data);
+                });
+            },
+            (next) => {
+                //Move them from the Backlog into a new column (Aging Backlog Items)
+                return next();
+            },
+            (next) => {
+                //Insert a "Refine Backlog" ticket into the "Ready" column
+                return next();
+            },
+            (next) => {
+                //Generate an email with the following info:
+                //All cards in the "Aging Backlog Items" column
+                //The total number of cards
+                return next();
+            }
+        ], (err) => {
+            if (err) {
+                console.error(err);
+                return callback(err);
+            }
+            return callback(null, 'Bert has finished, me lord');
+        });
 
-        //Move them from the Backlog into a new column (Aging Backlog Items)
-        //Insert a "Refine Backlog" ticket into the "Ready" column
-        //Generate an email with the following info:
-            //All cards in the "Aging Backlog Items" column
-            //The total number of cards
-
-    return trello.getBoardInfo((err, data) => {
-        if (err) {
-            return callback(err);
-        }
-
-        console.dir(data);
-        return callback(null, 'AWS CLI victory!');
-    });
+    //return trello.getBoardInfo((err, data) => {
+    //    if (err) {
+    //        return callback(err);
+    //    }
+    //
+    //    console.dir(data);
+    //    return callback(null, 'AWS CLI victory!');
+    //});
 }
--- trello_wrapper.js
+++ trello_wrapper.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const BOARD_ID = '54b4d5df9849cd0999ad99d4';
+
 let Trello = require('node-trello');
 let trello = new Trello("57a16d18be99f6b5a31d21c8e7c9f940", "4c5db157bae39369aa98c3e7427e13e6bba43f0ed233ea4c66e15438fb858f30");
+let datetime = require('node-datetime');
 
 
 let getBoardInfo = (cb) => {
@@ -16,6 +19,69 @@ let getBoardInfo = (cb) => {
     });
 };
 
+let getOldBacklogCards = (cb) => {
+    getBacklogListId((err, backlogListId) => {
+        if (err) {
+            return cb(err);
+        }
+
+        getBacklogCards(backlogListId, (err, cards) => {
+            if (err) {
+                return cb(err);
+            }
+
+            return cb(null, cards);
+        });
+    });
+};
+
+let getBacklogListId = (cb) => {
+    trello.get(`/1/boards/${BOARD_ID}/lists`, (err, lists) => {
+        if (err) {
+            console.error('Unable to retrieve backlog list ID');
+            console.error(err);
+            return cb(err);
+        }
+
+        for(var list of lists) {
+            if (list.name == "Backlog") {
+                return cb(null, list.id);
+            }
+        }
+    });
+};
+
+let getBacklogCards = (backlogListId, cb) => {
+
+    console.log(`Retrieving cards from Backlog (list ID: ${backlogListId})...`);
+
+    trello.get(`/1/lists/${backlogListId}/cards`, (err, cards) => {
+        if (err) {
+            console.error('Unable to retrieve cards from Backlog');
+            console.error(err);
+            return cb(err);
+        }
+
+        console.log(`Found ${cards.length} cards in Backlog list`);
+
+        let currentDateTime = datetime.create();
+        let twoWeeksInMillis = 1209600000;
+        let twoWeeksAgo = datetime.create(currentDateTime.getTime() - twoWeeksInMillis);
+        console.log(`Filtering cards untouched since ${twoWeeksAgo.format('d/m/Y H:M')}...`);
+
+
+        let untouchedCards = cards.filter((card) => {
+            let lastTouchedDateTime = datetime.create(card.dateLastActivity);
+            return lastTouchedDateTime.getTime() <= twoWeeksAgo.getTime();
+        });
+
+        console.log(`Found ${untouchedCards.length} untouched cards`);
+
+        return cb(null, cards);
+    });
+}
+
 module.exports = {
-    getBoardInfo
+    getBoardInfo,
+    getOldBacklogCards
 }
